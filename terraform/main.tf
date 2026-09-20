11cb2de827c5689b9c3c7f68aeffc6b4a66dd0ac
Abedullah Abu Touk <abdulah.abutouq@gmail.com>
2026-09-20 15:17:02 +0300

    Add Terraform configuration for control plane and worker virtual machines

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+module "tf_control_plane_01" {
+  source         = "./modules/vm"
+  vm_name        = "tf-control-plane-01"
+  vm_id          = 9001
+  node_name      = "pve"          # node .2
+  template_vm_id = 9000
+  ip_address     = "192.168.0.3/24"
+  gateway        = "192.168.0.1"
+  ssh_public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+module "tf_control_plane_02" {
+  source         = "./modules/vm"
+  vm_name        = "tf-control-plane-02"
+  vm_id          = 9002
+  node_name      = "pve-02"        # node .200
+  template_vm_id = 9100
+  ip_address     = "192.168.0.4/24"
+  gateway        = "192.168.0.1"
+  ssh_public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+module "tf_worker_01" {
+  source         = "./modules/vm"
+  vm_name        = "tf-worker-01"
+  vm_id          = 9003
+  node_name      = "pve"          # node .2
+  template_vm_id = 9000
+  ip_address     = "192.168.0.10/24"
+  gateway        = "192.168.0.1"
+  ssh_public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+module "tf_worker_02" {
+  source         = "./modules/vm"
+  vm_name        = "tf-worker-02"
+  vm_id          = 9004
+  node_name      = "pve-02"        # node .200
+  template_vm_id = 9100
+  ip_address     = "192.168.0.11/24"
+  gateway        = "192.168.0.1"
+  ssh_public_key = file("~/.ssh/id_ed25519.pub")
+}
